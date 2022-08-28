f4b0653882f89987bbc89c52689959eb16ed61ba
masud1989 <mranasrj@gmail.com>
2022-08-28 11:10:20 +0600

    login-success

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,4 +1,5 @@
 const UsersModel = require('../models/usersModel');
+const jwt = require('jsonwebtoken');
 
 // User Registration 
 exports.registration = (req, res) => {
@@ -11,4 +12,29 @@ exports.registration = (req, res) => {
             res.status(200).json({status: 'success', data:data})
         }
     })
+}
+
+// User Login
+exports.login = (req, res) => {
+    let reqBody = req.body;
+    UsersModel.aggregate(
+        [
+            {$match: reqBody},
+            {$project: {_id:0, email:1, name:1, address:1, mobile:1, photo:1 } }
+        ], (error, data) => {
+        if(error){
+            res.status(400).json({status: 'fail', data:error})
+        }
+        else{
+            if(data.length > 0){
+                let Payload = { exp: Math.floor(Date.now()/1000) + (24*36*36), data:data[0]['email'] }
+                let token = jwt.sign(Payload, 'SecretKey123456789');
+                res.status(200).json({ status: 'Success', token:token, data:data[0] })
+            }
+            else {
+                res.status(401).json({ status: 'Unauthorized' })
+            }
+        }
+    }
+    )
 }
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registration } = require('../controllers/UsersController');
+const { registration, login } = require('../controllers/UsersController');
 
 
 
 const router = express.Router();
 
 router.post('/registration', registration);
+router.post('/login', login);
 
 module.exports = router;
